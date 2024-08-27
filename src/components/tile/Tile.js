5c1c80ca902dd1d3069805da4a7900ab1fa080b8
camkol <koliyus3@hotmail.com>
2024-08-28 00:53:36 +0200

    use the object.values() method

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -3,10 +3,9 @@ import React from "react";
 export const Tile = ({ contacts }) => {
   return (
     <div className="tile-container">
-      <h2>{contacts.name}</h2>
-
-      <p>{contacts.phone}</p>
-      <p>{contacts.email}</p>
+      {Object.values(contacts).map((contact, index) => {
+        return <p key={index}>{contact}</p>;
+      })}
     </div>
   );
 };
