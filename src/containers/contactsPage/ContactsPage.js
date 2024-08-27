eb640b30cf57912a81e79380f313004b503ea5df
camkol <koliyus3@hotmail.com>
2024-08-27 09:13:34 +0200

    make duplicate checker and handlesubmit

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -8,6 +8,13 @@ export const ContactsPage = ({ onContact, contacts }) => {
   Define state variables for 
   contact info and duplicate check
   */
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  const isDuplicateName = (contactName) => {
+    return name.filter((nam) => nam.name === contactName);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,6 +22,19 @@ export const ContactsPage = ({ onContact, contacts }) => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    if (isDuplicateName(name)) {
+      return console.alert("Already in Contacts!");
+    }
+
+    const newContact = { name, phone, email };
+
+    onContact(newContact);
+
+    console.log(contacts);
+
+    setName("");
+    setPhone("");
+    setEmail("");
   };
 
   /*
@@ -25,7 +45,29 @@ export const ContactsPage = ({ onContact, contacts }) => {
   return (
     <div>
       <section>
-        <h2>Add Contact</h2>
+        <h2>Add Contact</h2>{" "}
+        <form onSubmit={handleSubmit}>
+          {" "}
+          <input
+            type="text"
+            placeholder="Name..."
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Phone..."
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Email..."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button>Add Contact</button>
+        </form>
       </section>
       <hr />
       <section>
